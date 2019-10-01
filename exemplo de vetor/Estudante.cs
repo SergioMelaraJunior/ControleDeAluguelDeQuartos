290d0bf95de414bc0c9029dc8ed0afa9ac547441
Sergio Melara Junior <sergio.melara@hotmail.com>
2019-10-01 08:50:03 -0300

    Aluguel de quartos

---
+++ exemplo de vetor/Estudante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exemplo_de_vetor {
+    class Estudante {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public Estudante(string nome, string email) {
+            Nome = nome;
+            Email = email;
+        }
+
+
+
+        public override string ToString() {
+            return "Nome: " + Nome + ", Email: " + Email;
+        }
+    }
+}
---
+++ exemplo de vetor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace exemplo_de_vetor {
+    class Program {
+        static void Main(string[] args) {
+            Estudante[] vect = new Estudante[10];
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel = {i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos Ocupados:");
+            for (int i = 0; i < 10; i++) {
+                if (vect[i] != null) {
+                    Console.WriteLine("Quarto " + i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
